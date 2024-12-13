90c673ec97846e327798d855579ef787cea8ead8
Roman Efremov <roman.efremov@jetbrains.com>
2024-12-13 11:50:46 +0100

    [FIR] Fix visibility of private top-level declaration from dangling file
    ...when checked from KaVisibilityChecker.
    
    The problem is that useSiteFile is from a dangling module, while
    declaration's containing file is from its context module.
    That's why simple `==` check is not enough, and we have to unwrap
    useSiteFile.
    In the case of code fragments, context file can be found by context
    element. In case of dangling files in completion we rely on
    `unwrapCopy()`.
    
    Q: Why does special handling for KaVisibilityChecker is needed? It
    worked fine in normal resolve.
    A: During resolve in IGNORE_SELF mode the useSiteFile will be replaced
    by the respective file from the source module by
    `patchDesignationPathForCopy()`. But this doesn't happen in
    `KaVisibilityChecker`.
    
    ^KTIJ-29375 Fixed

--- analysis/low-level-api-fir/src/org/jetbrains/kotlin/analysis/low/level/api/fir/providers/LLFirPrivateVisibleFromDifferentModuleExtension.kt
+++ analysis/low-level-api-fir/src/org/jetbrains/kotlin/analysis/low/level/api/fir/providers/LLFirPrivateVisibleFromDifferentModuleExtension.kt
@@ -5,25 +5,51 @@
 
 package org.jetbrains.kotlin.analysis.low.level.api.fir.providers
 
+import com.intellij.psi.PsiFile
 import org.jetbrains.kotlin.analysis.api.projectStructure.KaDanglingFileModule
 import org.jetbrains.kotlin.analysis.api.projectStructure.KaModule
 import org.jetbrains.kotlin.analysis.low.level.api.fir.projectStructure.LLFirModuleData
+import org.jetbrains.kotlin.analysis.low.level.api.fir.projectStructure.llFirModuleData
 import org.jetbrains.kotlin.analysis.low.level.api.fir.sessions.LLFirSession
+import org.jetbrains.kotlin.analysis.low.level.api.fir.util.unwrapCopy
 import org.jetbrains.kotlin.fir.FirModuleData
 import org.jetbrains.kotlin.fir.FirPrivateVisibleFromDifferentModuleExtension
+import org.jetbrains.kotlin.fir.declarations.FirFile
+import org.jetbrains.kotlin.fir.psi
+import org.jetbrains.kotlin.psi.KtCodeFragment
 
+/**
+ * [FirPrivateVisibleFromDifferentModuleExtension] which is aware of [KaDanglingFileModule]s.
+ * Dangling file can see private declarations of its respective context file.
+ */
 internal class LLFirPrivateVisibleFromDifferentModuleExtension(private val llFirSession: LLFirSession) :
     FirPrivateVisibleFromDifferentModuleExtension() {
 
-    /**
-     * When analyzing a dangling file we may resolve to declaration in the original module in case of
-     * [org.jetbrains.kotlin.analysis.api.projectStructure.KaDanglingFileResolutionMode.IGNORE_SELF] mode.
-     * Dangling file must be able to see private declarations of the original file, thus it needs special handling.
-     */
     override fun canSeePrivateDeclarationsOfModule(otherModuleData: FirModuleData): Boolean {
         check(otherModuleData is LLFirModuleData)
         return llFirSession.ktModule.unwrapDangling() == otherModuleData.ktModule
     }
 
     private fun KaModule.unwrapDangling(): KaModule = if (this is KaDanglingFileModule) contextModule else this
+
+    override fun canSeePrivateTopLevelDeclarationsFromFile(useSiteFile: FirFile, targetFile: FirFile): Boolean {
+        return useSiteFile.isDanglingFileWithContextFileEqualTo(targetFile)
+    }
+
+    private fun FirFile.isDanglingFileWithContextFileEqualTo(targetFile: FirFile): Boolean {
+        if (this.llFirModuleData.ktModule !is KaDanglingFileModule) return false
+        if (targetFile.llFirModuleData.ktModule is KaDanglingFileModule) return false
+
+        val unwrappedKtFile = findContextPsiFileForDanglingFirFile(useSiteFile = this)
+        return unwrappedKtFile != null && unwrappedKtFile == targetFile.psi
+    }
+
+    private fun findContextPsiFileForDanglingFirFile(useSiteFile: FirFile): PsiFile? {
+        val useSiteDanglingModule = useSiteFile.llFirModuleData.ktModule as? KaDanglingFileModule ?: return null
+        val danglingFile = useSiteDanglingModule.file
+        if (danglingFile is KtCodeFragment) {
+            danglingFile.context?.containingFile?.let { return it }
+        }
+        return danglingFile.unwrapCopy(danglingFile)
+    }
 }
