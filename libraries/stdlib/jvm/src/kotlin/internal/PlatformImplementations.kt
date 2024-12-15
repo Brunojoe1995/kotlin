5de344fb9441ac4934007ae37776918471390aea
Abduqodiri Qurbonzoda <abduqodiri.qurbonzoda@jetbrains.com>
2024-12-15 21:27:24 +0200

    Move java.time.Instant availability check to PlatformImplementations

--- libraries/stdlib/jvm/src/kotlin/internal/PlatformImplementations.kt
+++ libraries/stdlib/jvm/src/kotlin/internal/PlatformImplementations.kt
@@ -9,6 +9,7 @@ import java.lang.reflect.Method
 import java.util.regex.MatchResult
 import kotlin.random.FallbackThreadLocalRandom
 import kotlin.random.Random
+import kotlin.time.Clock
 
 internal open class PlatformImplementations {
 
@@ -43,6 +44,10 @@ internal open class PlatformImplementations {
     }
 
     public open fun defaultPlatformRandom(): Random = FallbackThreadLocalRandom()
+
+    public open fun getSystemClock(): Clock {
+        throw UnsupportedOperationException("getSystemClock should not be called on the base PlatformImplementations.")
+    }
 }
 
 
