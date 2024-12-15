4306ba91a4bf507c9be078f59bf75882f8bd1759
Abduqodiri Qurbonzoda <abduqodiri.qurbonzoda@jetbrains.com>
2024-12-15 23:01:19 +0200

    Merge ThreeTenBp tests into InstantTest

--- libraries/stdlib/test/time/InstantTest.kt
+++ libraries/stdlib/test/time/InstantTest.kt
@@ -204,4 +204,120 @@ class InstantTest {
         val min = Instant.fromEpochSeconds(-31619119219200L)
         assertEquals(max.epochSeconds - min.epochSeconds, (max - min).inWholeSeconds)
     }
+
+    @Test
+    fun compareInstants() {
+        val instants = arrayOf(
+            Instant.fromEpochSeconds(-2L, 0),
+            Instant.fromEpochSeconds(-2L, 999999998),
+            Instant.fromEpochSeconds(-2L, 999999999),
+            Instant.fromEpochSeconds(-1L, 0),
+            Instant.fromEpochSeconds(-1L, 1),
+            Instant.fromEpochSeconds(-1L, 999999998),
+            Instant.fromEpochSeconds(-1L, 999999999),
+            Instant.fromEpochSeconds(0L, 0),
+            Instant.fromEpochSeconds(0L, 1),
+            Instant.fromEpochSeconds(0L, 2),
+            Instant.fromEpochSeconds(0L, 999999999),
+            Instant.fromEpochSeconds(1L, 0),
+            Instant.fromEpochSeconds(2L, 0)
+        )
+        for (i in instants.indices) {
+            val a = instants[i]
+            for (j in instants.indices) {
+                val b = instants[j]
+                when {
+                    i < j -> {
+                        assertTrue(a < b, "$a <=> $b")
+                        assertNotEquals(a, b, "$a <=> $b")
+                    }
+                    i > j -> {
+                        assertTrue(a > b, "$a <=> $b")
+                        assertNotEquals(a, b, "$a <=> $b")
+                    }
+                    else -> {
+                        assertEquals(0, a.compareTo(b), "$a <=> $b")
+                        assertEquals(a, b, "$a <=> $b")
+                    }
+                }
+            }
+        }
+    }
+
+    @Test
+    fun instantEquals() {
+        val test5a: Instant = Instant.fromEpochSeconds(5L, 20)
+        val test5b: Instant = Instant.fromEpochSeconds(5L, 20)
+        val test5n: Instant = Instant.fromEpochSeconds(5L, 30)
+        val test6: Instant = Instant.fromEpochSeconds(6L, 20)
+        assertEquals(true, test5a == test5a)
+        assertEquals(true, test5a == test5b)
+        assertEquals(false, test5a == test5n)
+        assertEquals(false, test5a == test6)
+        assertEquals(true, test5b == test5a)
+        assertEquals(true, test5b == test5b)
+        assertEquals(false, test5b == test5n)
+        assertEquals(false, test5b == test6)
+        assertEquals(false, test5n == test5a)
+        assertEquals(false, test5n == test5b)
+        assertEquals(true, test5n == test5n)
+        assertEquals(false, test5n == test6)
+        assertEquals(false, test6 == test5a)
+        assertEquals(false, test6 == test5b)
+        assertEquals(false, test6 == test5n)
+        assertEquals(true, test6 == test6)
+    }
+
+    @Test
+    fun toEpochMilliseconds() {
+        assertEquals(Instant.fromEpochSeconds(1L, 1000000).toEpochMilliseconds(), 1001L)
+        assertEquals(Instant.fromEpochSeconds(1L, 2000000).toEpochMilliseconds(), 1002L)
+        assertEquals(Instant.fromEpochSeconds(1L, 567).toEpochMilliseconds(), 1000L)
+        assertEquals(Instant.fromEpochSeconds(Long.MAX_VALUE / 1_000_000).toEpochMilliseconds(), Long.MAX_VALUE / 1_000_000 * 1000)
+        assertEquals(Instant.fromEpochSeconds(Long.MIN_VALUE / 1_000_000).toEpochMilliseconds(), Long.MIN_VALUE / 1_000_000 * 1000)
+        assertEquals(Instant.fromEpochSeconds(0L, -1000000).toEpochMilliseconds(), -1L)
+        assertEquals(Instant.fromEpochSeconds(0L, 1000000).toEpochMilliseconds(), 1)
+        assertEquals(Instant.fromEpochSeconds(0L, 999999).toEpochMilliseconds(), 0)
+        assertEquals(Instant.fromEpochSeconds(0L, 1).toEpochMilliseconds(), 0)
+        assertEquals(Instant.fromEpochSeconds(0L, 0).toEpochMilliseconds(), 0)
+        assertEquals(Instant.fromEpochSeconds(0L, -1).toEpochMilliseconds(), -1L)
+        assertEquals(Instant.fromEpochSeconds(0L, -999999).toEpochMilliseconds(), -1L)
+        assertEquals(Instant.fromEpochSeconds(0L, -1000000).toEpochMilliseconds(), -1L)
+        assertEquals(Instant.fromEpochSeconds(0L, -1000001).toEpochMilliseconds(), -2L)
+    }
+
+    @Test
+    fun isLeapYear() {
+        assertEquals(false, isLeapYear(1999))
+        assertEquals(true, isLeapYear(2000))
+        assertEquals(false, isLeapYear(2001))
+        assertEquals(false, isLeapYear(2007))
+        assertEquals(true, isLeapYear(2008))
+        assertEquals(false, isLeapYear(2009))
+        assertEquals(false, isLeapYear(2010))
+        assertEquals(false, isLeapYear(2011))
+        assertEquals(true, isLeapYear(2012))
+        assertEquals(false, isLeapYear(2095))
+        assertEquals(true, isLeapYear(2096))
+        assertEquals(false, isLeapYear(2097))
+        assertEquals(false, isLeapYear(2098))
+        assertEquals(false, isLeapYear(2099))
+        assertEquals(false, isLeapYear(2100))
+        assertEquals(false, isLeapYear(2101))
+        assertEquals(false, isLeapYear(2102))
+        assertEquals(false, isLeapYear(2103))
+        assertEquals(true, isLeapYear(2104))
+        assertEquals(false, isLeapYear(2105))
+        assertEquals(false, isLeapYear(-500))
+        assertEquals(true, isLeapYear(-400))
+        assertEquals(false, isLeapYear(-300))
+        assertEquals(false, isLeapYear(-200))
+        assertEquals(false, isLeapYear(-100))
+        assertEquals(true, isLeapYear(0))
+        assertEquals(false, isLeapYear(100))
+        assertEquals(false, isLeapYear(200))
+        assertEquals(false, isLeapYear(300))
+        assertEquals(true, isLeapYear(400))
+        assertEquals(false, isLeapYear(500))
+    }
 }
