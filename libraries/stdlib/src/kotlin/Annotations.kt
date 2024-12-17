1282936bb6ad3ab34e0e20c3661e8a9e97010d7a
Filipp Zhinkin <filipp.zhinkin@jetbrains.com>
2024-12-17 13:54:25 -0500

    KT-71762 Fixed ReplaceWith's KDoc indentation
    ^KT-71762

--- libraries/stdlib/src/kotlin/Annotations.kt
+++ libraries/stdlib/src/kotlin/Annotations.kt
@@ -63,13 +63,13 @@ public annotation class DeprecatedSinceKotlin(
  * as IDEs can automatically apply the replacements specified through this annotation.
  *
  * @property expression the replacement expression. The replacement expression is interpreted in the context
- *     of the symbol being used, and can reference members of enclosing classes etc.
- *     For function calls, the replacement expression may contain argument names of the deprecated function,
- *     which will be substituted with actual parameters used in the call being updated. The imports used in the file
- *     containing the deprecated function or property are NOT accessible; if the replacement expression refers
- *     on any of those imports, they need to be specified explicitly in the [imports] parameter.
+ *   of the symbol being used, and can reference members of enclosing classes etc.
+ *   For function calls, the replacement expression may contain argument names of the deprecated function,
+ *   which will be substituted with actual parameters used in the call being updated. The imports used in the file
+ *   containing the deprecated function or property are NOT accessible; if the replacement expression refers
+ *   on any of those imports, they need to be specified explicitly in the [imports] parameter.
  * @property imports the qualified names that need to be imported in order for the references in the
- *     replacement expression to be resolved correctly.
+ *   replacement expression to be resolved correctly.
  */
 @Target()
 @Retention(BINARY)
